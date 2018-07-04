3820b990154ad3853dc22fb1e9f7a95c24f404e5
fudali <fuyi@23mofang.com>
2018-07-04 13:04:03 +0800

    feat: 添加 informers listers 依赖

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -1,30 +1,57 @@
 package runtime
 
 import (
+	"time"
+
 	"github.com/fudali113/good-job/typed"
-	"k8s.io/api/batch/v1"
-	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"github.com/fudali113/good-job/pkg/client/clientset/versioned"
+
+	kubeinformers "k8s.io/client-go/informers"
+	informers "github.com/fudali113/good-job/pkg/client/informers/externalversions"
 )
 
-type JobRuntime struct {
-	typed.Job
-	Status int           `json:"status"`
-	Logs   []interface{} `json:"logs"`
-}
+var clientset typed.Clientset
 
-type PipelineRuntime struct {
-	typed.Pipeline
-	CurrentIndex int           `json:"currentIndex"`
-	Status       int           `json:"status"`
-	Logs         []interface{} `json:"logs"`
-}
-
-type Runtime interface {
-	CreateJob(job typed.Job, exec typed.ExecConfig) (k8sJob *v1.Job, err error)
-	WatchJob(name string) (watch watch.Interface, err error)
-	CreateCronJob(resource, id, token string) error
-}
-
+// Start 根据 Config 运行 runtime
 func Start(config typed.RuntimeConfig)  {
-	
+	clientset.GoodjobV1alpha1().Jobs("")
+
+	informers.NewSharedInformerFactory(clientset.GoodJobClientset, 1 * time.Second)
+	kubeinformers.NewSharedInformerFactory(clientset.Clientset, 1 * time.Second)
 }
+
+func CreateClientset(token string) *typed.Clientset {
+	return &typed.Clientset{
+		Clientset: nil,
+		GoodJobClientset: nil,
+	}
+}
+
+func CreateOriginClientset(config *rest.Config, err error) (*kubernetes.Clientset, error) {
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(config)
+}
+
+func CreateGoodJobClientset(config *rest.Config, err error) (*versioned.Clientset, error) {
+	if err != nil {
+		return nil, err
+	}
+	return versioned.NewForConfig(config)
+}
+
+func CreteConfig(token string) (*rest.Config, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		return nil, err
+	}
+	if token != "" {
+		config.BearerToken = token
+	}
+	return config, err
+}
+
